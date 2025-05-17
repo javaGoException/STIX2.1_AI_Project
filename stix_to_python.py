694b767e153e75b7dad96c3a20dc139be1fff9a8
javaGoException <>
2025-05-17 23:13:42 +0200

    loading objects with all attributes

--- stix_to_python.py
+++ stix_to_python.py
@@ -19,32 +19,40 @@ driver = GraphDatabase.driver(uri=db_uri, auth=auth)
 
 #main function to load SDOs
 def load_sdos(path):
-    memory_store = MemoryStore()
-    memory_store.load_from_file(path)
+    # memory_store = MemoryStore()
+    # memory_store.load_from_file(path)
 
-    stix_objects_filter = [
-        Filter('type', '!=', 'relationship'),
-        Filter('type', '!=', 'x-mitre-collection')
-    ]
-    stix_objects = memory_store.query(stix_objects_filter)
+    # stix_objects_filter = [
+    #     Filter('type', '!=', 'relationship'),
+    #     Filter('type', '!=', 'x-mitre-collection')
+    # ]
+    # stix_objects = memory_store.query(stix_objects_filter)
+
+    with open(path) as f:
+        stix_json_data = json.load(f)
+
+    stix_objects = [obj for obj in stix_json_data["objects"] if obj["type"] not in ("relationship", "x-mitre-collection")]
 
     for stix_object in stix_objects:
 
         label = to_pascal_case(stix_object["type"])
         merge_query = f"""
-            MERGE (x:{label} {{id: '{stix_object["id"]}', type: '{stix_object["type"]}'}})
+            MERGE (x:{label} {{id: "{stix_object["id"]}", type: "{stix_object["type"]}"}})
         """
         session.run(merge_query)
 
         for attr, value in stix_object.items():
+            if isinstance(value, str):
+                value = double_to_single_quotes(value)
+
+            elif isinstance(value, (dict,list)):
+                value = double_to_single_quotes(json.dumps(value))
+
             match_query = f"""
-                MATCH (x:{label} {{id: '{stix_object["id"]}'}})
-                SET x.{attr} = '{json.dumps(value)}'
+                MATCH (x:{label} {{id: "{stix_object["id"]}"}})
+                SET x.{attr} = "{value}"
             """
             session.run(match_query)
-
-    # stix_relationships_filter = Filter('type', '==', 'relationship')
-    # stix_relationships = memory_store.query([stix_relationships_filter])
 
 
 def load_sros(path):
@@ -56,6 +64,11 @@ def to_pascal_case(input_string):
   pascal_case_string = "".join(word.capitalize() for word in words)
 
   return pascal_case_string
+
+def double_to_single_quotes(s):
+    s = s.replace('\\"', "'")
+    s = s.replace('"', "'")
+    return s
 
 
 with (driver.session(database=db_name) as session):
