ba9119e5a968d0c9a8bb19538c42de3416055cf4
javaGoException <>
2025-05-23 22:37:33 +0200

    loading Matrices To Tactics To Techniques embedded relationships

--- stix_to_neo.py
+++ stix_to_neo.py
@@ -41,7 +41,6 @@ def load_sdos(path):
         session.run(query, properties=object_properties)
 
 
-#TODO: Create a function for loading embedded relationships: Matrix, Tactics, Techniques
 #main function to load SROs
 def load_sros(path):
     with open(path) as f:
@@ -68,6 +67,70 @@ def load_sros(path):
         session.run(query, properties=relationship_properties)
 
 
+#main function to load embedded relationships
+def load_embedded_relationships(path):
+    with open(path) as f:
+        stix_json_data = json.load(f)
+
+    ###Matrices to Tactics###
+
+    matrix_objects = [obj for obj in stix_json_data["objects"] if obj["type"] == "x-mitre-matrix"]
+
+    for matrix_obj in matrix_objects:
+
+        for tactic_ref_id in matrix_obj["tactic_refs"]:
+
+            relationship_type = "ReferencesTactic"
+
+            relationship_properties = {
+                "relationship_type": relationship_type,
+                "source_ref": matrix_obj["id"],
+                "target_ref": tactic_ref_id
+            }
+
+            query = f"""
+                MATCH (sourceObject {{id: "{matrix_obj["id"]}"}}), (targetObject {{id: "{tactic_ref_id}"}})
+                MERGE (sourceObject)-[r:{relationship_type}]->(targetObject)
+                SET r = $properties
+            """
+            session.run(query, properties=relationship_properties)
+
+    ###Tactics to Techniques###
+
+    tactic_shortname_to_id = {}
+    for obj in stix_json_data["objects"]:
+        if obj["type"] == "x-mitre-tactic" and "x_mitre_shortname" in obj:
+            tactic_shortname_to_id[obj["x_mitre_shortname"]] = obj["id"]
+
+    attack_patterns = [obj for obj in stix_json_data["objects"] if obj["type"] == "attack-pattern"]
+
+    for attack_pattern in attack_patterns:
+        attack_pattern_id = attack_pattern["id"]
+
+        if attack_pattern.get("kill_chain_phases"):
+            for phase in attack_pattern["kill_chain_phases"]:
+                phase_name = phase["phase_name"]
+
+                if phase_name in tactic_shortname_to_id:
+                    tactic_id = tactic_shortname_to_id[phase_name]
+
+                    relationship_type = "IsTacticOf"
+
+                    relationship_properties = {
+                        "relationship_type": relationship_type,
+                        "source_ref": tactic_id,
+                        "target_ref": attack_pattern_id,
+                        "kill_chain_name": phase.get("kill_chain_name")
+                    }
+
+                    query = f"""
+                            MATCH (sourceObject {{id: "{tactic_id}"}}), (targetObject {{id: "{attack_pattern_id}"}})
+                            MERGE (sourceObject)-[r:{relationship_type}]->(targetObject)
+                            SET r = $properties
+                        """
+                    session.run(query, properties=relationship_properties)
+
+
 def to_pascal_case(input_string):
   words = input_string.split('-')
   pascal_case_string = "".join(word.capitalize() for word in words)
@@ -82,18 +145,21 @@ with (driver.session(database=db_name) as session):
         # print_results(results)
         load_sdos(path)
         load_sros(path)
+        load_embedded_relationships(path)
 
     def load_mobile(path: str):
         # results = validate_file(path)
         # print_results(results)
         load_sdos(path)
         load_sros(path)
+        load_embedded_relationships(path)
 
     def load_enterprise(path: str):
         # results = validate_file(path)
         # print_results(results)
         load_sdos(path)
         load_sros(path)
+        load_embedded_relationships(path)
 
     load_ics("attack-stix-data/ics-attack-17.1.json")
     load_mobile("attack-stix-data/mobile-attack-17.1.json")
